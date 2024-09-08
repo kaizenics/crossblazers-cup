abe40ef179732127cbe8b5b273857580e849aedb
kaizenics <nicosejohnsoriano@gmail.com>
2024-09-09 01:53:07 +0800

    feat: Add Carousel component and ThemeProvider component

---
+++ src/components/ui/theme-provider.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { type ThemeProviderProps } from "next-themes/dist/types"
+
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  return (
+    <NextThemesProvider
+      attribute="class"
+      defaultTheme="dark"
+      enableSystem={false}
+      forcedTheme="dark"
+      disableTransitionOnChange
+      {...props}
+    >
+      {children}
+    </NextThemesProvider>
+  )
+}
