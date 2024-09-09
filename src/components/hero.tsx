a1b8ea95a57cad60ef4dcbe8dd049cbe1ac81720
kaizenics <nicosejohnsoriano@gmail.com>
2024-09-10 04:31:00 +0800

    refactor: Update About and Hero components
    - Refactor the About component to use the new Container component for improved layout.
    - Update the Hero component to include event details with icons for location and date.

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
+import { RiMapPin2Fill, RiCalendarFill } from "react-icons/ri";
 import heroCover from "@/assets/cover/hero.jpg";
 
 import ccje from "@/assets/logo/ccje.png";
@@ -22,18 +23,17 @@ export const Hero = () => {
   };
 
   return (
-    <>
-      <div className="relative w-full h-screen">
-       
+    <div className="relative w-full h-screen flex flex-col justify-center items-center">
+      <div className="absolute inset-0">
         <Image
-          className="absolute top-0 left-0 w-full h-full object-cover opacity-50"
+          className="w-full h-full object-cover opacity-70"
           src={heroCover}
           alt="Hero Cover"
         />
         <div className="absolute bottom-0 left-0 w-full h-screen bg-gradient-to-b from-transparent to-[#0a0a0a] opacity-100"></div>
       </div>
 
-      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white space-y-2 flex flex-col items-center">
+      <div className="z-10 text-white space-y-2 flex flex-col items-center">
         <div className="flex flex-row space-x-4 mb-3">
           <Image
             className="justify-center items-center"
@@ -85,28 +85,43 @@ export const Hero = () => {
             height={60}
           />
         </div>
-    
-          <h1 className="text-7xl font-raceSport text-center">
-            Cross Blazers Cup 2024
+
+        <div className="container space-y-2 pb-2">
+          <h1 className="text-8xl font-raceSport text-center">
+            Cross Blazers Cup
           </h1>
           <p className="text-xl font-montserrat text-center">
-            Join the exciting event only here at Holy Cross of Davao College
+            Join us for an unforgettable experience filled with excitement,
+            sportsmanship, and community pride!
           </p>
-     
+        </div>
+        <div className="flex flex-row space-x-2 font-montserrat">
+          <div className="border border-white/20 bg-white/20 backdrop-blur-md rounded-lg py-2 px-5">
+            <div className="flex space-x-2">
+              <RiMapPin2Fill className="w-5 h-5" />
+              <p>HCDC Student Lounge</p>
+            </div>
+          </div>
+          <div className="border border-white/20 bg-white/20 backdrop-blur-md rounded-lg py-2 px-5">
+          <div className="flex space-x-2">
+              <RiCalendarFill className="w-5 h-6" />
+              <p>November 2024</p>
+            </div>
+          </div>
+        </div>
         <div className="flex flex-row space-x-2">
           <Button
-            className={`relative overflow-hidden font-montserrat text-lg px-8 my-5 ${
+            className={`relative overflow-hidden font-montserrat text-lg font-semibold uppercase py-7 px-8 my-5 ${
               isShimmering
                 ? "animate-shimmer bg-gradient-to-r from-primary via-primary-foreground to-primary bg-[length:400%_100%]"
                 : ""
             }`}
             onClick={handleShimmerClick}
           >
-            Play Mini Game
+            Event Details
           </Button>
         </div>
       </div>
-     
-    </>
+    </div>
   );
 };
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -38,37 +38,7 @@ export const Navbar = () => {
         </div>
       </nav>
 
-      <div
-        className={`fixed top-15 transform ${
-          showSidebar ? "translate-x-0" : "-translate-x-full"
-        } transition-transform duration-500 ease-in-out z-10 w-screen h-screen text-white dark:text-white bg-black bg-opacity-70 backdrop-filter backdrop-blur-lg`}
-      >
-        <div className="flex flex-col justify-center items-center">
-      
-        </div>
-        <div className="flex justify-center items-center mt-10 mb-20 space-x-72">
-          <ul className="text-2xl text-center uppercase space-y-7 text-zinc-400 cursor-pointer">
-            <li>News</li>
-            <li>Live</li>
-            <li>Discography</li>
-            <li>Video</li>
-            <li>Biography</li>
-            <li>Goods</li>
-            <li>Official Home</li>
-          </ul>
-          <ul className="text-2xl text-center uppercase space-y-7 text-zinc-400 cursor-pointer">
-            <li>News</li>
-            <li>Live</li>
-            <li>Discography</li>
-            <li>Video</li>
-            <li>Biography</li>
-            <li>Goods</li>
-            <li>Official Home</li>
-          </ul>
-        </div>
-
-
-      </div>
+    
     </>
   );
 };
