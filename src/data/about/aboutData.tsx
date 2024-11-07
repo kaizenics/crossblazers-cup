9941ac93e5326812607499e3233b6875eb0423f0
kaizenics <nicosejohnsoriano@gmail.com>
2024-11-07 15:05:53 +0800

    feat: Refactor About component and move data to separate file for better organization

---
+++ src/data/about/aboutData.tsx
@@ -0,0 +1,153 @@
+import { IconNews } from "@tabler/icons-react";
+import oration from "@/assets/images/about/oration.jpg";
+import bballWomen from "@/assets/images/about/basketball-women.jpg";
+import bballMen from "@/assets/images/about/basketball-men.jpg";
+import sing from "@/assets/images/about/sing.jpg";
+import esports from "@/assets/images/about/esports.jpg";
+import chess from "@/assets/images/about/chess.jpg";
+import storytelling from "@/assets/images/about/storytelling.jpg";
+import takraw from "@/assets/images/about/takraw.jpg";
+import cheerdance from "@/assets/images/about/cheerdance.jpg";
+import dancesport from "@/assets/images/about/dance-sports.jpg";
+import folkdance from "@/assets/images/about/folk-dance.jpg";
+import vballMen from "@/assets/images/about/volleyball-men.jpg";
+import megacrew from "@/assets/images/about/megacrew.jpg";
+import tennisMen from "@/assets/images/about/table-tennis.jpg";
+import quizbowl from "@/assets/images/about/quizbowl.jpg";
+import filmMaking from "@/assets/images/about/filmmaking.png";
+
+export const items = [
+  {
+    title: "Cheerdance",
+    description: "Athletic dance and team spirit combined",
+    imageSrc: cheerdance,
+    imageAlt: "Age of Enlightenment concept image",
+    className: "md:col-span-2",
+    icon: <IconNews className="h-4 w-4 text-gray-400" />,
+  },
+  {
+    title: "Oration",
+    description: "Inspiring speeches and powerful messages",
+    imageSrc: oration,
+    imageAlt: "Newly crowned queen image",
+    className: "md:col-span-1",
+    icon: <IconNews className="h-4 w-4 text-gray-400" />,
+  },
+  {
+    title: "Arise and Sing",
+    description: "Listen to the sweet melodies of the night.",
+    imageSrc: sing,
+    imageAlt: "Art of Design concept image",
+    className: "md:col-span-1",
+    icon: <IconNews className="h-4 w-4 text-gray-400" />,
+  },
+  {
+    title: "Esports",
+    description: "Experience the thrill of the game.",
+    imageSrc: esports,
+    imageAlt: "Power of Communication concept image",
+    className: "md:col-span-2",
+    icon: <IconNews className="h-4 w-4 text-gray-400" />,
+  },
+
+  {
+    title: "Basketball Men",
+    description: "Catch the action",
+    imageSrc: bballMen,
+    imageAlt: "Age of Enlightenment concept image",
+    className: "md:col-span-2",
+    icon: <IconNews className="h-4 w-4 text-gray-400" />,
+  },
+  {
+    title: "Chess",
+    description: "Strategic board game for the mind",
+    imageSrc: chess,
+    imageAlt: "Newly crowned queen image",
+    className: "md:col-span-1",
+    icon: <IconNews className="h-4 w-4 text-gray-400" />,
+  },
+
+  {
+    title: "Story Telling",
+    description: "Engaging narratives and creative storytelling",
+    imageSrc: storytelling,
+    imageAlt: "Art of Design concept image",
+    className: "md:col-span-1",
+    icon: <IconNews className="h-4 w-4 text-gray-400" />,
+  },
+  {
+    title: "Sepak Takraw",
+    description: "Thrilling sport of agility and reflexes",
+    imageSrc: takraw,
+    imageAlt: "Power of Communication concept image",
+    className: "md:col-span-2",
+    icon: <IconNews className="h-4 w-4 text-gray-400" />,
+  },
+
+  {
+    title: "Basketball Women",
+    description: "Agility and teamwork in the court",
+    imageSrc: bballWomen,
+    imageAlt: "Age of Enlightenment concept image",
+    className: "md:col-span-2",
+    icon: <IconNews className="h-4 w-4 text-gray-400" />,
+  },
+  {
+    title: "Dance Sports",
+    description: "Graceful and competitive",
+    imageSrc: dancesport,
+    imageAlt: "Newly crowned queen image",
+    className: "md:col-span-1",
+    icon: <IconNews className="h-4 w-4 text-gray-400" />,
+  },
+  {
+    title: "Volleyball Men",
+    description: "Powerful and dynamic plays",
+    imageSrc: vballMen,
+    imageAlt: "Art of Design concept image",
+    className: "md:col-span-1",
+    icon: <IconNews className="h-4 w-4 text-gray-400" />,
+  },
+  {
+    title: "Phillippine Folk Dance",
+    description: "Cultural heritage in motion",
+    imageSrc: folkdance,
+    imageAlt: "Power of Communication concept image",
+    className: "md:col-span-2",
+    icon: <IconNews className="h-4 w-4 text-gray-400" />,
+  },
+
+  {
+    title: "Mega Crew",
+    description: "Energetic and synchronized",
+    imageSrc: megacrew,
+    imageAlt: "Age of Enlightenment concept image",
+    className: "md:col-span-2",
+    icon: <IconNews className="h-4 w-4 text-gray-400" />,
+  },
+  {
+    title: "Table Tennis Men",
+    description: "Fast-paced and precise",
+    imageSrc: tennisMen,
+    imageAlt: "Newly crowned queen image",
+    className: "md:col-span-1",
+    icon: <IconNews className="h-4 w-4 text-gray-400" />,
+  },
+  {
+    title: "Quiz Bowl",
+    description: "Testing knowledge and quick thinking",
+    imageSrc: quizbowl,
+    imageAlt: "Art of Design concept image",
+    className: "md:col-span-1",
+    icon: <IconNews className="h-4 w-4 text-gray-400" />,
+  },
+  {
+    title: "Film Making",
+    description: "Experience the thrill of the game.",
+    imageSrc: filmMaking,
+    imageAlt: "Power of Communication concept image",
+    className: "md:col-span-2",
+    icon: <IconNews className="h-4 w-4 text-gray-400" />,
+  },
+
+];
