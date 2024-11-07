d11691a792307835ffc7ee4974432d424997657a
kaizenics <nicosejohnsoriano@gmail.com>
2024-11-07 15:34:28 +0800

    feat: Add new event items and images to About component; update Footer layout and styling

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,60 +2,71 @@ import { Container } from "@/components/ui/container";
 
 export const Footer = () => {
   return (
-    <>
-      <section className="mt-56 sm:mt-0">
-        <div className="w-[100%] h-[1.5px] bg-white/20">
-          <div className="px-2" />
-        </div>
-        <Container variant={"fullMobileBreakpointPadded"}>
-          <footer>
-            <div className="sm:flex sm:flex-wrap sm:space-x-36">
-              <div>
-                <div className="sm:mx-[10px] my-[8%] align-left space-y-4">
-                  <h1 className="font-montserrat font-semibold text-lg sm:mb-4 text-white content-start">
-                    Resources
-                  </h1>
-                  <div className="font-montserrat text-md items-start text-[#afaeaedc] inline-block mb-[5px]">
-                    <p className="mb-3">Help</p>
-                    <p className="mb-3">Privacy</p>
-                    <p className="mb-3">FAQ</p>
-                  </div>
-                </div>
-                <div className="sm:mx-[10px] my-[8%] align-left space-y-4">
-                  <h1 className="font-montserrat font-semibold text-lg sm:mb-4 text-white content-start">
-                    Socials
-                  </h1>
-                  <div className="font-montserrat text-md items-start text-[#afaeaedc] inline-block mb-[5px] whitespace-nowrap">
-                    <a
-                      href="https://www.facebook.com/hcdcits"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <p className="mb-3 hover:underline hover:text-white">
-                        Facebook
-                      </p>
-                    </a>
-                    <p className="mb-3">Instagram</p>
-                    <a
-                      href="https://github.com/hcdc-its"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <p className="mb-3">GitHub</p>
-                    </a>
-                  </div>
-                </div>
-              </div>
+    <section className="mt-12 sm:mt-24 md:mt-56">
+      <div className="w-full h-[1.5px] bg-white/20" />
+      <Container variant="fullMobileBreakpointPadded">
+        <footer className="py-8 sm:py-12">
+          <div className="flex flex-col sm:flex-row sm:flex-wrap justify-between">
+            <div className="w-full sm:w-auto mt-8 sm:mt-0">
+              <p className="font-raceSport text-3xl sm:text-5xl md:text-5xl text-center sm:text-right">
+                Cross Blazers Cup
+              </p>
+            </div>
 
-              <div className="relative hidden md:block w-[300px] h-[50px] sm:w-[650px] sm:h-[100px] my-10 sm:my-28">
-                <p className="font-raceSport text-6xl text-center">
-                  Cross Blazers Cup
-                </p>
+            <div className="flex flex-row space-x-28 sm:space-x-16 mb-8 sm:mb-0 justify-center pt-10 sm:pt-0">
+              <div className="space-y-4">
+                <h2 className="font-montserrat font-semibold text-lg text-white">
+                  Resources
+                </h2>
+                <ul className="font-montserrat text-sm sm:text-md text-[#afaeaedc] space-y-2">
+                  <li>
+                    <a href="#" className="hover:text-white hover:underline">
+                      Help
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="hover:text-white hover:underline">
+                      Privacy
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="hover:text-white hover:underline">
+                      FAQ
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div className="space-y-4">
+                <h2 className="font-montserrat font-semibold text-lg text-white">
+                  Socials
+                </h2>
+                <ul className="font-montserrat text-sm sm:text-md text-[#afaeaedc] space-y-2">
+                  <li>
+                    <a
+                      href="https://www.facebook.com/hcdcssgexed"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="hover:text-white hover:underline"
+                    >
+                      Facebook
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="hover:text-white hover:underline">
+                      Instagram
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="hover:text-white hover:underline">
+                      GitHub
+                    </a>
+                  </li>
+                </ul>
               </div>
             </div>
-          </footer>
-        </Container>
-      </section>
-    </>
+          </div>
+        </footer>
+      </Container>
+    </section>
   );
 };
