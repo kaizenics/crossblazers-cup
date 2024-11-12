0650266afe1c5139cff98084786245b553796f32
kaizenics <nicosejohnsoriano@gmail.com>
2024-11-12 08:11:38 +0800

    refactor: Remove unused MDX components and clean up Next.js configuration

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ['lh3.googleusercontent.com'],
-  },
-}
-
-module.exports = nextConfig
+    images: {
+      domains: ['lh3.googleusercontent.com'],
+    },
+  }
+  
+  module.exports = nextConfig
